f602d283b14caba715960940bf27d2225151bf88
Brandon Choi <bchoi0824@gmail.com>
2026-02-12 21:37:56 -0700

    Fixed bugs

--- requirements.R
+++ requirements.R
@@ -1,4 +1,4 @@
-p <- c("tidyverse", "shiny", "leaflet", "httr2", "googlePolylines", "lpSolve", "shinydashboard", "plotly")
+p <- c("tidyverse", "shiny", "leaflet", "httr2", "googlePolylines", "lpSolve", "shinydashboard", "plotly", "s2", "units", "raster", "sf")
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
     install.packages(new.packages, dependencies = TRUE, repos = "https://cloud.r-project.org")
