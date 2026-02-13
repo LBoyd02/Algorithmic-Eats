9e1eddb325513238588713b1fa23c207d1fc2f2f
LBoyd02 <liamboyd@shaw.ca>
2026-02-12 19:46:42 -0700

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,5 +1,5 @@
 p <- c("tidyverse", "shiny", "leaflet", "httr2", "googlePolylines", "lpSolve", "shinydashboard", "plotly")
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
-    install.packages(new.packages, dependencies = TRUE)
+    install.packages(new.packages, dependencies = TRUE, repos = "https://cloud.r-project.org")
 }
